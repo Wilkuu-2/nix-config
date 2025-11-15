81fde581e996434f4d6fe90963c7824669ac0b01
Jakub Stachurski <j.stachurski@student.utwente.nl>
2025-11-15 22:08:20 +0100

    Add system prompter

--- home-modules/services/default.nix
+++ home-modules/services/default.nix
@@ -1,6 +1,7 @@
-{lib, ...}: {
+{lib, pkgs ,...}: {
    imports = [./vdirsyncer.nix ./direnv.nix ./syncthing.nix];
 
   # Gnome keyring, very smort 
   services.gnome-keyring.enable = true;
+  home.packages = [ pkgs.gcr ]; 
 } 
