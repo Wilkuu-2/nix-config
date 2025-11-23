2572453be432e4ff4bac017703fd7214bcf63c39
Jakub Stachurski <j.stachurski@student.utwente.nl>
2025-11-23 16:13:24 +0100

    Move home definition to users

--- users/live-user.nix
+++ users/live-user.nix
@@ -22,6 +22,22 @@
     ];
   };
 
-  home-manager.users.live-user.imports = [ ../home/live-user.nix ];
+  home-manager.users.live-user = {
+    imports = [ ../home-modules ];
 
+    homeapps = { 
+      nvim.enable = true; 
+      nvim.lsp = false;
+
+      presets = {
+        browser.enable = true;
+        utils.enable = true;
+      };
+    };
+
+    home.username = "live-user";
+    home.homeDirectory = "/home/live-user";
+    home.stateVersion = "24.11";
+    programs.home-manager.enable = true;
+  };
 }
--- users/wilkuu.nix
+++ users/wilkuu.nix
@@ -27,6 +27,28 @@
     ];
   };
 
-  home-manager.users.wilkuu.imports = [ ../home/wilkuu.nix ];
+  home-manager.users.wilkuu = {
+    imports = [ ../home-modules ];
+
+    homeapps.presets.full.enable = true;
+    # homeapps.vnc = true;
+
+    services.wayvnc = {
+      enable = true;
+      autoStart = false;
+      settings = {
+        # Todo, bind to vpn and LAN explicitly somehow
+        address = "0.0.0.0";
+        port = 5900;
+      };
+    };
+
+    homesv.vdirsyncer.enable = true;
+
+    home.username = "wilkuu";
+    home.homeDirectory = "/home/wilkuu";
+    home.stateVersion = "24.11";
+    programs.home-manager.enable = true;
+  }; 
 
 }
